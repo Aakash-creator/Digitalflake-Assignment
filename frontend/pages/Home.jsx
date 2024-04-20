3ac984894531ae4a4485335f46cd23518d1926c2
Aakash <aakashmaneak@gmail.com>
2024-04-20 12:42:52 +0530

    added home page

---
+++ frontend/pages/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <>
+      <div className="flex items-center  my-6 justify-center mb-6 text-2xl  text-gray-900 dark:text-white">
+        <img className="w-8 flex items-center h-8 mr-2" src="../public\vite.svg" alt="" />
+        <hr />
+        <div>
+          <span className=" font-semibold">digital</span>flake
+        </div>
+        <p className="  flex justify-center">Welcome to Digitalflake Admin</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;

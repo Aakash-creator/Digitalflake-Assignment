d1c859d84f95f7c39c8ebfd6e5286ebdfafc6660
Aakash <aakashmaneak@gmail.com>
2024-04-20 12:34:06 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,33 @@
+import AddCategory from "../component/AddCategory";
+import AddProducts from "../component/AddProducts";
+import Layout from "../component/Layout";
+import Sidebar from "../component/Sidebar";
+import Category from "../pages/Category";
+import Dashboard from "../pages/Home";
+import PageNotFound from "../pages/PageNotFound";
+import Products from "../pages/Products";
+import SignIn from "../pages/SignIn";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <>DIGITAL FLAKE HACKATHON</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route element={<Layout />}>
+          <Route element={<Sidebar />}>
+            <Route path="/home" element={<Dashboard />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/category/addcategory" element={<AddCategory />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/addproducts" element={<AddProducts />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
